76cf29364630a89ff58c37d352a737c33e4d8090
sebastian <sebastian.soecker@gmail.comm>
2025-01-17 07:17:03 -0600

    Remove exportPathMap config - not compatible with app directory

--- website/next.config.ts
+++ website/next.config.ts
@@ -38,33 +38,6 @@ const nextConfig: NextConfig = {
   },
   basePath: '',
   assetPrefix: '',
-  // Add exportPathMap to explicitly define all static pages
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-      '/planner': { page: '/planner' },
-      '/states': { page: '/states' },
-      '/datenschutz': { page: '/datenschutz' },
-      '/impressum': { page: '/impressum' },
-      '/app': { page: '/app-redirect' },
-      '/states/berlin': { page: '/states/[state]', query: { state: 'berlin' } },
-      '/states/bayern': { page: '/states/[state]', query: { state: 'bayern' } },
-      '/states/baden-wuerttemberg': { page: '/states/[state]', query: { state: 'baden-wuerttemberg' } },
-      '/states/brandenburg': { page: '/states/[state]', query: { state: 'brandenburg' } },
-      '/states/bremen': { page: '/states/[state]', query: { state: 'bremen' } },
-      '/states/hamburg': { page: '/states/[state]', query: { state: 'hamburg' } },
-      '/states/hessen': { page: '/states/[state]', query: { state: 'hessen' } },
-      '/states/mecklenburg-vorpommern': { page: '/states/[state]', query: { state: 'mecklenburg-vorpommern' } },
-      '/states/niedersachsen': { page: '/states/[state]', query: { state: 'niedersachsen' } },
-      '/states/nordrhein-westfalen': { page: '/states/[state]', query: { state: 'nordrhein-westfalen' } },
-      '/states/rheinland-pfalz': { page: '/states/[state]', query: { state: 'rheinland-pfalz' } },
-      '/states/saarland': { page: '/states/[state]', query: { state: 'saarland' } },
-      '/states/sachsen': { page: '/states/[state]', query: { state: 'sachsen' } },
-      '/states/sachsen-anhalt': { page: '/states/[state]', query: { state: 'sachsen-anhalt' } },
-      '/states/schleswig-holstein': { page: '/states/[state]', query: { state: 'schleswig-holstein' } },
-      '/states/thueringen': { page: '/states/[state]', query: { state: 'thueringen' } },
-    };
-  }
 };
 
 export default nextConfig;
